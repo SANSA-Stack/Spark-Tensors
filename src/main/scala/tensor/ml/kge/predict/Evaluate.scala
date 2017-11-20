252ee428897d35419fa84d5b25b4a74e13e8ff18
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-11-20 10:30:26 +0100

    fix Evaluation

--- src/main/scala/tensor/ml/kge/predict/Evaluate.scala
+++ src/main/scala/tensor/ml/kge/predict/Evaluate.scala
@@ -2,10 +2,12 @@ package tensor.ml.kge.predict
 
 import org.apache.spark.sql._
 
-import tensor.ml.kge.dataset._
 import tensor.ml.kge.models._
+import tensor.ml.kge.dataset.Dataset
 
-class Evaluate(model: TransE, test: DataFrame, sk: SparkSession) extends Predict(test: DataFrame) {
+class Evaluate(model: TransE, test: Dataset, sk: SparkSession) extends Predict(test: Dataset) {
+
+  import sk.implicits._
 
   def head(i: Int, r: Row) = {
     Row(i, r.getInt(1), r.getInt(2))
@@ -21,7 +23,7 @@ class Evaluate(model: TransE, test: DataFrame, sk: SparkSession) extends Predict
     val y = model.myL(model.dist(row))
 
     x = y +: x
-    for (i <- 1 until test.count().toInt) {
+    for (i <- 1 until test.s.length) {
       x = model.myL(model.dist(head(i, row))) +: x
     }
 
@@ -34,7 +36,7 @@ class Evaluate(model: TransE, test: DataFrame, sk: SparkSession) extends Predict
     val y = model.myL(model.dist(row))
 
     x = y +: x
-    for (i <- 1 until test.count().toInt) {
+    for (i <- 1 until test.s.length) {
       x = model.myL(model.dist(tail(i, row))) +: x
     }
 
