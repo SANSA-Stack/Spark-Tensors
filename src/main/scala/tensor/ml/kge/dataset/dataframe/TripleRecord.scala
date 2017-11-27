5314e08c3ab097f2801cc530404007b07eb7a425
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-11-27 17:11:06 +0100

    add string and integer record

---
+++ src/main/scala/tensor/ml/kge/dataset/dataframe/TripleRecord.scala
@@ -0,0 +1,5 @@
+package tensor.ml.kge.dataset.dataframe
+
+case class StringRecord(Subject: String, Predicate: String, Object: String)
+
+case class IntegerRecord(Subject: Int, Predicate: Int, Object: Int)
