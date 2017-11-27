2d98e131e0e41cc8ce4ccb66cacb436e7725b9b9
lpfgarcia@gmail.com <lpfgarcia@gmail.com>
2017-11-27 10:50:56 +0100

    remove sparkSession

--- src/main/scala/tensor/ml/kge/crossValidation/Bootstrapping.scala
+++ src/main/scala/tensor/ml/kge/crossValidation/Bootstrapping.scala
@@ -2,12 +2,9 @@ package tensor.ml.kge.crossValidation
 
 import org.apache.spark.sql._
 
-class Bootstrapping(data: DataFrame, sk: SparkSession) extends CrossValidation {
-
-  import sk.implicits._
+class Bootstrapping(data: DataFrame) extends CrossValidation {
 
   def crossValidation() = {
-
     val train = data.sample(true, 1).toDF()
     val test = data.except(train).toDF()
     (train, test)
--- src/main/scala/tensor/ml/kge/crossValidation/Holdout.scala
+++ src/main/scala/tensor/ml/kge/crossValidation/Holdout.scala
@@ -4,15 +4,12 @@ import org.apache.spark.sql._
 
 case class rateException(info: String) extends Exception
 
-class Holdout(data: DataFrame, rate: Float, sk: SparkSession) extends CrossValidation {
-
-  import sk.implicits._
+class Holdout(data: DataFrame, rate: Float) extends CrossValidation {
 
   if (rate < 0 || rate >= 1)
     throw new rateException("Rate value should be higher than 0 and lower than 1")
 
   def crossValidation() = {
-
     val train = data.sample(false, rate).toDF()
     val test = data.except(train).toDF()
     (train, test)
